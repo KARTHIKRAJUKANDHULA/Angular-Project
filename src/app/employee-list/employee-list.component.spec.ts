dd59cd0060587f39439d1293f7bd3223110b7a34
KARTHIKRAJUKANDHULA <karthik901069@gmail.com>
2021-07-28 17:31:41 +0530

    commit

--- src/app/employee-list/employee-list.component.spec.ts
+++ src/app/employee-list/employee-list.component.spec.ts
@@ -1,14 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 
 import { EmployeeListComponent } from './employee-list.component';
 
-describe('EmployeeListComponent', () => {
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { EmployeeService } from '../employee.service';
+//import { EmployeeService } from 'src/app/employee.service.ts';
+
+
+fdescribe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
   let fixture: ComponentFixture<EmployeeListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EmployeeListComponent ]
+      imports: [ RouterTestingModule,HttpClientTestingModule,FormsModule],
+      declarations: [ EmployeeListComponent]
     })
     .compileComponents();
   });
@@ -22,4 +31,18 @@ describe('EmployeeListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call getOperatorDetail  and return [] ',fakeAsync(()=>{
+    fixture = TestBed.createComponent(EmployeeListComponent);
+    let component=fixture.debugElement.componentInstance;
+    let empService=fixture.debugElement.injector.get(EmployeeService);
+    spyOn(empService,"getEmployeeList").and.callFake
+    {return of(expect(component.tempOperator).toEqual({ "statusCode":200 }));}
+    
+    component.getEmployeeList();
+    
+    expect(component.tempOperator).toEqual({ "statusCode":200 });
+    
+  })) 
+
 });
